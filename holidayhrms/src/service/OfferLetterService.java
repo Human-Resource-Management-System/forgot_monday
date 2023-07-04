bf69a6c6e2771f2f1515820fdadc495ae07d3429
yahudhussain.s <yahudhussain.s@PENNANT-VSP205.pennant.local>
2023-07-04 13:42:48 +0530

    offer_tuesday

--- holidayhrms/src/service/OfferLetterService.java
+++ holidayhrms/src/service/OfferLetterService.java
@@ -2,6 +2,8 @@ package service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,8 @@ import models.InductionDocumentTypes;
 import models.OfferModel;
 
 public class OfferLetterService {
+	private final Logger logger = LoggerFactory.getLogger(OfferLetterService.class);
+
 	private OfferLetterDAO offerLetterDAO;
 
 	@Autowired
@@ -26,20 +30,28 @@ public class OfferLetterService {
 
 	@Transactional
 	public void updateEmploymentOfferDocuments(HrmsEmploymentOffer employmentOfferModel, OfferModel of) {
+		logger.info("Updating employment offer documents");
+		logger.debug("Getting the candidate ID from the employment offer model");
 
 		System.out.println("in here");
 		// getting eofrId
 		int eofrId = employmentOfferModel.getCandidateId();
 		// getting the list of documents should bring by candidate
+		logger.debug("Getting the list of documents to bring by the candidate");
 		List<String> documentsToBring = of.getDocuments();
+		logger.debug("Documents to bring: {}", documentsToBring);
 
 		System.out.println(documentsToBring + "newly updarted code");
+		logger.debug("Fetching the induction documents from the database");
 		// setting inductionDocumentTypes model from inductionDocumentTypes table
 		List<InductionDocumentTypes> inductionDocuments = offerLetterDAO.getInductionDocuments();
+		logger.debug("Induction documents: {}", inductionDocuments);
 
 		System.out.println(inductionDocuments);
 		int docIndex = 1;
 		for (String document : documentsToBring) {
+			logger.debug("Processing document: {}", document);
+
 			// getting IdtyId by the document name
 			int idtyId = offerLetterDAO.findIdtyIdByTitle(inductionDocuments, document);
 
@@ -50,12 +62,15 @@ public class OfferLetterService {
 			empoffdocComposite.setDocumentIndex(docIndex);
 			employmentofferdocument.setEmpoff(empoffdocComposite);
 			employmentofferdocument.setOfferidentity(idtyId);
+			logger.debug("Updated employment offer document: {}", employmentofferdocument);
 
 			System.out.println(employmentofferdocument);
 			// update the data into data base which got from entity model of employmentofferdocuments
 			offerLetterDAO.updateEmploymentOfferDocuments(employmentofferdocument);
 			docIndex++;
 		}
+		logger.info("Employment offer documents updated successfully");
+
 	}
 
 }
